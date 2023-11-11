3fe8742058ea9cc86a6ea3d607c9b8be637753d9
Ivan Korbakov <korbakovi@gmail.com>
2023-11-11 18:45:37 +0100

    add .env for react

--- frontend/src/components/Api.js
+++ frontend/src/components/Api.js
@@ -1,5 +1,5 @@
 // components/Api.js
-const BASE_URL = process.env.API_URL;
+const BASE_URL = process.env.REACT_APP_API_URL;
 const getHeaders = () => {
     const authToken = localStorage.getItem('authToken');
     return {
