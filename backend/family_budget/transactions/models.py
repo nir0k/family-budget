77d7d021617a673e1c0512c65e7f598a1d6a2b8b
Ivan Korbakov <korbakovi@gmail.com>
2023-12-30 23:09:29 +0100

    Change transfer transaction and chnage frontent

--- backend/family_budget/transactions/models.py
+++ backend/family_budget/transactions/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models, transaction
 
 from accounts.models import Account
@@ -30,13 +31,14 @@ class Transaction_Type(models.Model):
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=100, unique=True)
+    title = models.CharField(max_length=100)
     type = models.ForeignKey(Transaction_Type,
                              verbose_name='Type',
                              on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['title']
+        unique_together = (('title', 'type'),)
 
     def __str__(self):
         return self.title
@@ -78,86 +80,162 @@ class Transaction(models.Model):
         on_delete=models.CASCADE,
         db_index=True
     )
+    isTransfer = models.BooleanField(default=False)
+    transfer_group_id = models.UUIDField(default=uuid.uuid4, editable=False)
 
     class Meta:
         ordering = ['-date']
 
     @transaction.atomic
     def save(self, *args, **kwargs):
+        handle_related = kwargs.pop('handle_related', True)
+        is_new = not self.pk
+        was_transfer = False
+
+        if is_new and self.type.type == "=":
+            self.isTransfer = True
+            title = (
+                f'from {self.account.owner.username}-{self.account.title} '
+                f'to {self.account_to.owner.username}-{self.account_to.title}'
+            )
+            transfer_group_id = (
+                self.transfer_group_id
+                if self.transfer_group_id else uuid.uuid4())
+
+            Transaction(
+                title=title,
+                type=Transaction_Type.objects.get(type="-"),
+                category=Category.objects.get(type__type="-",
+                                              title="Transfer"),
+                who=self.who,
+                account=self.account,
+                amount=self.amount,
+                currency=self.account.currency,
+                description=self.description,
+                author=self.author,
+                date=self.date,
+                account_to=self.account_to,
+                isTransfer=True,
+                transfer_group_id=transfer_group_id,
+            ).save()
+
+            Transaction(
+                title=title,
+                type=Transaction_Type.objects.get(type="+"),
+                category=Category.objects.get(type__type="+",
+                                              title="Transfer"),
+                who=self.account_to.owner,
+                account=self.account_to,
+                amount=self.amount,
+                currency=self.account.currency,
+                description=self.description,
+                author=self.author,
+                date=self.date,
+                account_to=self.account,
+                isTransfer=True,
+                transfer_group_id=transfer_group_id,
+            ).save()
+            return
+
+        if not is_new and handle_related:
+            old_instance = Transaction.objects.get(pk=self.pk)
+            was_transfer = (old_instance.isTransfer
+                            and old_instance.transfer_group_id)
+            account_to_changed = (old_instance.account_to != self.account_to
+                                  or old_instance.account != self.account)
+
+        if not is_new and was_transfer:
+            if self.isTransfer and self.transfer_group_id:
+                related_transactions = Transaction.objects.filter(
+                    transfer_group_id=self.transfer_group_id
+                ).exclude(pk=self.pk)
+                changed_fields = {
+                    field.name: getattr(self, field.name)
+                    for field in self._meta.fields
+                    if getattr(old_instance, field.name)
+                    != getattr(self, field.name)
+                }
+
+                for trans in related_transactions:
+                    for field_name, value in changed_fields.items():
+                        if (field_name == 'amount'
+                                and self.type.title != trans.type.title):
+                            value = value
+                        setattr(trans, field_name, value)
+                    trans.amount_converted = self.convert_amount(
+                        amount=trans.amount,
+                        from_currency=trans.currency,
+                        to_currency=trans.account.currency,
+                        date=trans.date
+                    )
+                    fields_to_update = (
+                        list(changed_fields.keys()) + ['amount_converted',])
+                    if account_to_changed:
+                        fields_to_update = (
+                            list(changed_fields.keys()) + [
+                                'account', 'amount_converted', 'currency'])
+                        trans.account_to = self.account
+                        trans.account = self.account_to
+                        if trans.type.type == "-":
+                            trans.currency = trans.account.currency
+                        trans.amount_converted = self.convert_amount(
+                            amount=trans.amount,
+                            from_currency=trans.currency,
+                            to_currency=trans.account.currency,
+                            date=trans.date
+                        )
+                    trans.save(handle_related=False,
+                               update_fields=fields_to_update)
+                if self.type.type == "+":
+                    self.currency = self.account_to.currency
+
+        if self.pk:
+            oldTrans = Transaction.objects.get(pk=self.pk)
+            oldAmount = oldTrans.amount_converted
+            if oldTrans.type.title == "Income":
+                oldAmount *= -1
+            oldTrans.account.balance += oldAmount
+            oldTrans.account.save()
+
         self.type = self.category.type
-        converted_amount = self.convert_amount(
+        self.account.refresh_from_db()
+        self.amount_converted = self.convert_amount(
             amount=self.amount,
             from_currency=self.currency,
             to_currency=self.account.currency,
             date=self.date
         )
-        self.amount_converted = converted_amount
-        if self.type.title == 'Transfer':
-            converted_amount_to = self.convert_amount(
-                amount=converted_amount,
-                from_currency=self.account.currency,
-                to_currency=self.account_to.currency,
-                date=self.date
-            )
-            self.amount_converted_to = converted_amount_to
-
-        if self.pk:
-            old_trans = Transaction.objects.get(pk=self.pk)
-            if self.type.title == 'Income':
-                self.account.balance -= old_trans.amount_converted
-            elif self.type.title == 'Expense':
-                self.account.balance += old_trans.amount_converted
-            elif self.type.title == 'Transfer':
-                if old_trans.account != self.account:
-                    old_trans.account.balance += old_trans.amount_converted
-                    old_trans.account.save()
-                else:
-                    self.account.balance += old_trans.amount_converted
-                    self.account.save()
-                if old_trans.account_to != self.account_to:
-                    old_trans.account_to.balance -= (
-                        old_trans.amount_converted_to)
-                    old_trans.account_to.save()
-                else:
-                    self.account_to.balance -= old_trans.amount_converted_to
-                    self.account.save()
-
-        if self.type.title == 'Income':
-            self.account.balance += converted_amount
-            self.account.save()
-        elif self.type.title == 'Expense':
-            self.account.balance -= converted_amount
-            self.account.save()
-        elif self.type.title == 'Transfer':
-            if self.account == self.account_to:
-                raise ValueError(
-                    'Both source and destination accounts must'
-                    ' be specified and be different for a transfer.')
-            if not self.account and not self.account_to:
-                raise ValueError(
-                    'Both source and destination accounts'
-                    ' must be specified for a transfer.')
-            self.account.balance -= converted_amount
-            self.account.save()
-            self.account_to.balance += converted_amount_to
-            self.account_to.save()
+        amount = self.amount_converted
+        if self.type.title == 'Expense':
+            amount *= -1
+        self.account.balance += amount
+        self.account.save()
 
         super().save(*args, **kwargs)
 
     @transaction.atomic
     def delete(self, *args, **kwargs):
-        if self.type.title == 'Income':
-            self.account.balance -= self.amount_converted
+        handle_related = kwargs.pop('handle_related', True)
+        if handle_related:
+            amount = self.amount_converted
+            if self.category.type.type == "+":
+                amount *= -1
+            self.account.balance += amount
             self.account.save()
-        elif self.type.title == 'Expense':
-            self.account.balance += self.amount_converted
-            self.account.save()
-        elif self.type.title == 'Transfer':
-            if self.account and self.account_to:
-                self.account.balance += self.amount_converted
-                self.account_to.balance -= self.amount_converted_to
-                self.account.save()
-                self.account_to.save()
+
+        if self.isTransfer and self.transfer_group_id and handle_related:
+            related_transactions = Transaction.objects.filter(
+                transfer_group_id=self.transfer_group_id
+            ).exclude(pk=self.pk)
+
+            for trans in related_transactions:
+                if trans.category.type.type == "+":
+                    trans.account.balance -= trans.amount_converted
+                else:
+                    trans.account.balance += trans.amount_converted
+                trans.account.save()
+
+                trans.delete(handle_related=False)
 
         super().delete(*args, **kwargs)
 
--- backend/family_budget/transactions/tests/test_transactions.py
+++ backend/family_budget/transactions/tests/test_transactions.py
@@ -21,14 +21,21 @@ class AccountBalanceTest(TestCase):
             email="user1@test.com",
             password="P@ssw0rd1"
         )
+
         self.t_type_expense = Transaction_Type.objects.create(
             title="Expense", type="-")
         self.t_type_income = Transaction_Type.objects.create(
             title="Income", type="+")
         self.t_type_transfer = Transaction_Type.objects.create(
             title="Transfer", type="=")
+        Category.objects.create(
+            title="Transfer", type=Transaction_Type.objects.get(type="+"))
+        Category.objects.create(
+            title="Transfer", type=Transaction_Type.objects.get(type="-"))
         self.category_expense = Category.objects.create(
             title="Food", type=Transaction_Type.objects.get(title="Expense"))
+        self.category_expense2 = Category.objects.create(
+            title="Games", type=Transaction_Type.objects.get(title="Expense"))
         self.category_income = Category.objects.create(
             title="Sallary", type=Transaction_Type.objects.get(title="Income"))
         self.category_transfer = Category.objects.create(
@@ -81,6 +88,20 @@ class AccountBalanceTest(TestCase):
             rate_date=self.date
         )
 
+        ExchangeRate.objects.create(
+            from_currency=self.currency2,
+            to_currency=self.currency3,
+            rate=8,
+            rate_date=self.date
+        )
+
+        ExchangeRate.objects.create(
+            from_currency=self.currency3,
+            to_currency=self.currency2,
+            rate=9,
+            rate_date=self.date
+        )
+
     def test_income_transaction_in_same_currency(self):
         """Test creating, edit and delete income transaction
         with same currency"""
@@ -96,16 +117,23 @@ class AccountBalanceTest(TestCase):
             date=self.date
         )
         self.account1.refresh_from_db()
+        t = Transaction.objects.get(pk=transaction.pk)
         self.assertEqual(self.account1.balance, Decimal('150.00'))
+        self.assertEqual(t.amount, Decimal('50.00'))
+        self.assertEqual(t.amount_converted, Decimal('50.00'))
 
         transaction.amount = 30
         transaction.save()
         self.account1.refresh_from_db()
+        t = Transaction.objects.get(pk=transaction.pk)
         self.assertEqual(self.account1.balance, Decimal('130.00'))
+        self.assertEqual(t.amount, Decimal('30.00'))
+        self.assertEqual(t.amount_converted, Decimal('30.00'))
 
         transaction.delete()
         self.account1.refresh_from_db()
         self.assertEqual(self.account1.balance, Decimal('100.00'))
+        self.assertFalse(Transaction.objects.filter(pk=transaction.pk))
 
     def test_expense_transaction_in_same_currency(self):
         """Test creating, edit and delete expense transaction
@@ -122,22 +150,54 @@ class AccountBalanceTest(TestCase):
             date=self.date
         )
         self.account1.refresh_from_db()
+        t = Transaction.objects.get(pk=transaction.pk)
         self.assertEqual(self.account1.balance, Decimal('70.00'))
+        self.assertEqual(t.amount, Decimal('30.00'))
+        self.assertEqual(t.amount_converted, Decimal('30.00'))
 
         transaction.amount = 50
         transaction.save()
         self.account1.refresh_from_db()
+        t = Transaction.objects.get(pk=transaction.pk)
         self.assertEqual(self.account1.balance, Decimal('50.00'))
+        self.assertEqual(t.amount, Decimal('50.00'))
+        self.assertEqual(t.amount_converted, Decimal('50.00'))
+
+        transaction.category = self.category_expense2
+        transaction.save()
+        self.account1.refresh_from_db()
+        self.assertEqual(self.account1.balance, Decimal('50.00'))
+
+        transaction.category = self.category_income
+        transaction.save()
+        self.account1.refresh_from_db()
+        t = Transaction.objects.get(pk=transaction.pk)
+        self.assertEqual(self.account1.balance, Decimal('150.00'))
+        self.assertEqual(t.amount, Decimal('50.00'))
+        self.assertEqual(t.amount_converted, Decimal('50.00'))
 
         transaction.delete()
         self.account1.refresh_from_db()
         self.assertEqual(self.account1.balance, Decimal('100.00'))
+        self.assertFalse(Transaction.objects.filter(pk=transaction.pk))
 
     def test_transfer_transaction_in_same_currency(self):
         """Test creating, edit and delete transfer transaction
         with same currency"""
-        transaction = Transaction.objects.create(
-            title='Test transfer account 1 in USD to account 2 EUR',
+        user2 = User.objects.create(
+            username="User2",
+            email="user2@test.com",
+            password="P@ssw0rd2"
+        )
+        user2account = Account.objects.create(
+            title="User2 Account USD",
+            balance=Decimal('100.00'),
+            type=Account_Type.objects.get(title="Card"),
+            currency=self.currency1,
+            owner=user2
+        )
+        Transaction.objects.create(
+            title='Test transfer account 1 in USD to User 2 account in USD',
             type=self.t_type_transfer,
             category=self.category_transfer,
             who=self.user,
@@ -146,26 +206,46 @@ class AccountBalanceTest(TestCase):
             currency=self.currency1,
             author=self.user,
             date=self.date,
-            account_to=self.account2
+            account_to=user2account
         )
         self.account1.refresh_from_db()
-        self.account2.refresh_from_db()
+        user2account.refresh_from_db()
+        trans1 = Transaction.objects.get(type__type="-",
+                                         category__title="Transfer")
+        trans2 = Transaction.objects.get(type__type="+",
+                                         category__title="Transfer")
         self.assertEqual(self.account1.balance, Decimal('70.00'))
-        self.assertEqual(self.account2.balance, Decimal('260.00'))
+        self.assertEqual(user2account.balance, Decimal('130.00'))
+        self.assertEqual(trans1.amount, Decimal('30.00'))
+        self.assertEqual(trans2.amount, Decimal('30.00'))
+        self.assertEqual(trans1.amount_converted, Decimal('30.00'))
+        self.assertEqual(trans2.amount_converted, Decimal('30.00'))
 
+        transaction = Transaction.objects.get(type__type="-",
+                                              category__title="Transfer")
         transaction.amount = 15
         transaction.save()
         self.account1.refresh_from_db()
-        self.account2.refresh_from_db()
+        user2account.refresh_from_db()
+        trans1 = Transaction.objects.get(type__type="-",
+                                         category__title="Transfer")
+        trans2 = Transaction.objects.get(type__type="+",
+                                         category__title="Transfer")
         self.assertEqual(self.account1.balance, Decimal('85.00'))
-        self.assertEqual(self.account2.balance, Decimal('230.00'))
+        self.assertEqual(user2account.balance, Decimal('115.00'))
+        self.assertEqual(trans1.amount, Decimal('15.00'))
+        self.assertEqual(trans2.amount, Decimal('15.00'))
+        self.assertEqual(trans1.amount_converted, Decimal('15.00'))
+        self.assertEqual(trans2.amount_converted, Decimal('15.00'))
 
         transaction.delete()
 
         self.account1.refresh_from_db()
-        self.account2.refresh_from_db()
+        user2account.refresh_from_db()
         self.assertEqual(self.account1.balance, Decimal('100.00'))
-        self.assertEqual(self.account2.balance, Decimal('200.00'))
+        self.assertEqual(user2account.balance, Decimal('100.00'))
+        self.assertFalse(
+            Transaction.objects.filter(category__title="Transfer"))
 
     def test_income_transaction_in_diferent_currency(self):
         """Test creating, edit and delete income transaction
@@ -223,37 +303,80 @@ class AccountBalanceTest(TestCase):
         """Test creating, edit and delete an transfer transaction
         with different currency"""
         transaction = Transaction.objects.create(
-            title='Test transfer in HUF from account 1 USD to account 2 EUR',
+            title='Test transfer from account 1 USD to account 2 EUR',
             type=self.t_type_transfer,
             category=self.category_transfer,
             who=self.user,
             account=self.account1,
             amount=Decimal('30.00'),
-            currency=self.currency3,
+            currency=self.currency2,
             author=self.user,
             date=self.date,
             account_to=self.account2
         )
         self.account1.refresh_from_db()
         self.account2.refresh_from_db()
-        self.assertEqual(self.account1.balance, Decimal('-80.00'))
-        self.assertEqual(self.account2.balance, Decimal('560.00'))
+        trans1 = Transaction.objects.get(type__type="-",
+                                         category__title="Transfer")
+        trans2 = Transaction.objects.get(type__type="+",
+                                         category__title="Transfer")
+        self.assertEqual(self.account1.balance, Decimal('70.00'))
+        self.assertEqual(self.account2.balance, Decimal('260.00'))
+        self.assertEqual(trans1.amount, Decimal('30.00'))
+        self.assertEqual(trans1.currency.code, "USD")
+        self.assertEqual(trans1.amount, Decimal('30.00'))
+        self.assertEqual(trans2.currency.code, "USD")
+        self.assertEqual(trans1.amount_converted, Decimal('30.00'))
+        self.assertEqual(trans2.amount_converted, Decimal('60.00'))
 
+        transaction = Transaction.objects.get(type__type="-",
+                                              category__title="Transfer")
         transaction.amount = 15
         transaction.save()
         self.account1.refresh_from_db()
         self.account2.refresh_from_db()
-        self.assertEqual(self.account1.balance, Decimal('10.00'))
-        self.assertEqual(self.account2.balance, Decimal('380.00'))
+        trans1 = Transaction.objects.get(type__type="-",
+                                         category__title="Transfer")
+        trans2 = Transaction.objects.get(type__type="+",
+                                         category__title="Transfer")
+        self.assertEqual(self.account1.balance, Decimal('85.00'))
+        self.assertEqual(self.account2.balance, Decimal('230.00'))
+        self.assertEqual(trans1.amount, Decimal('15.00'))
+        self.assertEqual(trans1.currency.code, "USD")
+        self.assertEqual(trans1.amount, Decimal('15.00'))
+        self.assertEqual(trans2.currency.code, "USD")
+        self.assertEqual(trans1.amount_converted, Decimal('15.00'))
+        self.assertEqual(trans2.amount_converted, Decimal('30.00'))
 
+        transaction = Transaction.objects.get(type__type="-",
+                                              category__title="Transfer")
         transaction.account_to = self.account3
         transaction.save()
         self.account1.refresh_from_db()
         self.account2.refresh_from_db()
         self.account3.refresh_from_db()
-        self.assertEqual(self.account1.balance, Decimal('10.00'))
+        trans1 = Transaction.objects.get(type__type="-",
+                                         category__title="Transfer")
+        trans2 = Transaction.objects.get(type__type="+",
+                                         category__title="Transfer")
+        self.assertEqual(self.account1.balance, Decimal('85.00'))
         self.assertEqual(self.account2.balance, Decimal('200.00'))
-        self.assertEqual(self.account3.balance, Decimal('1000630.00'))
+        self.assertEqual(self.account3.balance, Decimal('1000105.00'))
+
+        # transaction = Transaction.objects.get(type__type="+",
+        #                                       category__title="Transfer")
+        # transaction.account_to = self.account2
+        # transaction.save()
+        # self.account1.refresh_from_db()
+        # self.account2.refresh_from_db()
+        # self.account3.refresh_from_db()
+        # trans1 = Transaction.objects.get(type__type="-",
+        #                                  category__title="Transfer")
+        # trans2 = Transaction.objects.get(type__type="+",
+        #                                  category__title="Transfer")
+        # self.assertEqual(self.account1.balance, Decimal('100.00'))
+        # self.assertEqual(self.account2.balance, Decimal('185.00'))
+        # self.assertEqual(self.account3.balance, Decimal('1000120.00'))
 
         transaction.delete()
 
@@ -263,3 +386,49 @@ class AccountBalanceTest(TestCase):
         self.assertEqual(self.account1.balance, Decimal('100.00'))
         self.assertEqual(self.account2.balance, Decimal('200.00'))
         self.assertEqual(self.account3.balance, Decimal('1000000.00'))
+
+    def test_create_and_delete_transfer_transaction_in_diferent_currency1(
+            self):
+        """Test creating, edit and delete an transfer transaction
+        with different currency"""
+        transaction = Transaction.objects.create(
+            title='Test transfer from account 1 USD to account 2 EUR',
+            type=self.t_type_transfer,
+            category=self.category_transfer,
+            who=self.user,
+            account=self.account1,
+            amount=Decimal('30.00'),
+            currency=self.currency2,
+            author=self.user,
+            date=self.date,
+            account_to=self.account2
+        )
+        self.account1.refresh_from_db()
+        self.account2.refresh_from_db()
+        trans1 = Transaction.objects.get(type__type="-",
+                                         category__title="Transfer")
+        trans2 = Transaction.objects.get(type__type="+",
+                                         category__title="Transfer")
+        self.assertEqual(self.account1.balance, Decimal('70.00'))
+        self.assertEqual(self.account2.balance, Decimal('260.00'))
+        self.assertEqual(trans1.amount, Decimal('30.00'))
+        self.assertEqual(trans1.currency.code, "USD")
+        self.assertEqual(trans1.amount, Decimal('30.00'))
+        self.assertEqual(trans2.currency.code, "USD")
+        self.assertEqual(trans1.amount_converted, Decimal('30.00'))
+        self.assertEqual(trans2.amount_converted, Decimal('60.00'))    
+
+        transaction = Transaction.objects.get(type__type="+",
+                                              category__title="Transfer")
+        transaction.account_to = self.account3
+        transaction.save()
+        self.account1.refresh_from_db()
+        self.account2.refresh_from_db()
+        self.account3.refresh_from_db()
+        trans1 = Transaction.objects.get(type__type="-",
+                                         category__title="Transfer")
+        trans2 = Transaction.objects.get(type__type="+",
+                                         category__title="Transfer")
+        self.assertEqual(self.account1.balance, Decimal('100.00'))
+        self.assertEqual(self.account2.balance, Decimal('470.00'))
+        self.assertEqual(self.account3.balance, Decimal('999970.00'))
